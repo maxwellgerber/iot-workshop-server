99ae6612ccdc064446ddd19b3071c5cff2eac515
Maxwell Gerber <Maxwell.Gerber@blackbaud.com>
2017-03-23 23:08:01 -0700

    Improved error messages

--- client.py
+++ client.py
@@ -3,9 +3,9 @@ import serial
 import argparse
 
 #read in port and station
-parser = argparse.ArgumentParser(description='Process station number and port number.')
-parser.add_argument('--station')
-parser.add_argument('--port')
+parser = argparse.ArgumentParser(description='Python utility for HKN IoT Workshop.')
+parser.add_argument('--station', help="The station number you are at", type=int, required=True)
+parser.add_argument('--port', help="The com port of the MSP you are using", required=True)
 args = parser.parse_args()
 station = int(args.station)
 port = args.port
@@ -15,6 +15,6 @@ ser = serial.Serial(port)
 with SocketIO('hkn-iot-workshop.herokuapp.com', 80) as sock:
 	while True:
 		value = ser.readline()
-		value = value.decode("utf-8").strip() 
-		print(value)
-		sock.emit('data', {'i':station, 'd':value}) 
+		cleaned = value.decode("utf-8").strip() 
+		print("Transmittiong {}".format(cleaned))
+		sock.emit('data', {'i':station, 'd':cleaned}) 
