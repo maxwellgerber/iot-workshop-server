6c7b36126be7db97ef288105bd42fd93a081c2e9
Maxwell Gerber <mgerber@berkeley.edu>
2017-03-23 21:06:40 -0700

    modify port

--- client.py
+++ client.py
@@ -1,22 +1,49 @@
-import socket
-import random
-import serial
-import time
+from socketIO_client import SocketIO
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(('localhost', 7000))
-ser = serial.Serial("/dev/cu.usbmodem14223")
+def on_connect():
+    print('connect')
+
+def on_disconnect():
+    print('disconnect')
+
+def on_reconnect():
+    print('reconnect')
+
+def on_aaa_response(*args):
+    print('on_aaa_response', args)
+
+print('shit')
+socketIO = SocketIO('hkn-iot-workshop.herokuapp.com', 80)
+socketIO.emit('data', {'i':1, 'd': 4}) 
+socketIO.emit('data', {'i':2, 'd': 4000}) 
+socketIO.emit('data', {'i':3, 'd': 2333}) 
+socketIO.emit('data', {'i':4, 'd': 400}) 
+socketIO.emit('data', {'i':4, 'd': 33}) 
+socketIO.emit('data', {'i':5, 'd': .34}) 
+socketIO.emit('data', {'i':6, 'd': .114}) 
+socketIO.wait(seconds=5)
+print('shit')
+
+# print("shit")
+# import serial
+# import socket
+# import random
+# import time
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.connect(('10.142.38.175', 7000))
+# ser = serial.Serial("/dev/cu.usbmodem14223")
 
 
-def iot(index, value):
-    msg = '{"index": '+str(index)+', "value": '+str(value).strip()+'}'
-    print "sending {}".format(msg)
-    s.send(str.encode(msg))
+# def iot(index, value):
+#     msg = '{"index": '+str(index)+', "value": '+str(value).strip()+'}'
+#     print "sending {}".format(msg)
+#     s.send(str.encode(msg))
 
-def test():
-    while True:
-        value = ser.readline()
-        print value
-        iot(1,value)
+# def test():
+#     while True:
+#         value = ser.readline()
+#         print value
+#         iot(1,value)
 
-test()
+# test()
--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 var express = require('express')
 var app = express()
-var server = app.listen(8080, function () {
+
+app.set('port', (process.env.PORT || 5000));
+
+var server = app.listen(app.get('port'), function () {
 	console.log('HTTP server listening on port 8080')
 });
-var io = require('socket.io').listen(server)
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html');
-});
 
-var udp = require('dgram').createSocket('udp4');
-udp.on('listening', function () {
-    var address = udp.address();
-    console.log('UDP Server listening on ' + address.address + ":" + address.port);
-});
-udp.on('message', function (message, remote) {
-	var msg = JSON.parse(message)
-	data[msg.index] = msg.value;
-});
-udp.bind(7000, "localhost");
+var io = require('socket.io').listen(server);
+
+app.use(express.static('.'));
 
 var data = new Array(40);
 for (var i = 0; i < data.length; i++) {
 	data[i] = -1;
 }
+
+io.on('connection', function (socket){
+	socket.on('data', function (d) {
+		console.log(d);
+		data[d.i] = d.d;
+	})
+})
 
 setInterval(function () { io.emit('update', data); }, 33);
 
